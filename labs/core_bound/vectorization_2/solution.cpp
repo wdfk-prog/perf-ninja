4e0dd9f7bc13058ab2c8277a004c798f001a08b3
Liya Huang <1425075683@qq.com>
2025-04-13 10:26:01 +0800

    fix:修复checksum函数，使用uint32_t累加器以避免溢出并优化返回值计算

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,19 @@
 #include "solution.hpp"
 
-uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
+uint16_t checksum(const Blob& blob) {
+  uint32_t acc = 0;
+  for (const auto value : blob) {
     acc += value;
-    acc += acc < value; // add carry
   }
-  return acc;
+
+  auto high = acc >> 16;
+  auto low = acc & 0xFFFFu;
+  // 包装加法以确保结果适合 16 位，用于校验和计算。
+  acc = low + high;
+
+  high = acc >> 16;
+  low = acc & 0xFFFFu;
+  acc = low + high;
+  // 校验和算法通过迭代对 16 位高电平和低电平部分求和，将 32 位累加器减少到 16 位，直到没有溢出。
+  return static_cast<uint16_t>(acc);
 }
